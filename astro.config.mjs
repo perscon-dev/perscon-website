923dae49eb787afe981a91c3aae08b40e3fe2c1b
perscon-dev <contacto.perscon@gmail.com>
2026-09-11 10:42:49 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://www.perscon.com.mx',
+  site: 'https://perscon.com.mx',
 
   vite: {
     plugins: [tailwindcss()],
